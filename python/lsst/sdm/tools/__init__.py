9392c986e019b5728d7f59dde8111bfaac8e10d7
Jeremy McCormick <jeremym@slac.stanford.edu>
2025-02-19 13:09:10 -0600

    Change python module from lsst.sdm_tools to lsst.sdm.tools

--- python/lsst/sdm/tools/__init__.py
+++ python/lsst/sdm/tools/__init__.py
@@ -1,4 +1,4 @@
-# This file is part of sdm-tools.
+# This file is part of sdm_tools.
 #
 # Developed for the LSST Data Management System.
 # This product includes software developed by the LSST Project
